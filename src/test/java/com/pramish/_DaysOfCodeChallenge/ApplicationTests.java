7d8989fd12143fb345ee049d49c60ab9c657457c
Pramish Thapa <pramish.thapa17@gmail.com>
2024-10-06 18:29:46 +0100

    Initial commit

---
+++ src/test/java/com/pramish/_DaysOfCodeChallenge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramish._DaysOfCodeChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
