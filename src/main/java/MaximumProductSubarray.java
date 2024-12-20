37d2006b9037a41157ac773dc066191deb8d2f98
Pramish Thapa <pramish.thapa17@gmail.com>
2024-12-20 19:56:59 +0000

    Maximum Product Subarray code updated

--- src/main/java/MaximumProductSubarray.java
+++ src/main/java/MaximumProductSubarray.java
@@ -6,7 +6,7 @@ public class MaximumProductSubarray {
         for (int num : nums) {
             int tmp = curMax * num;
             curMax = Math.max(Math.max(num * curMax, num * curMin), num);
-            curMin = Math.min(Math.min(tmp,m * curMin), num);
+            curMin = Math.min(Math.min(tmp, num * curMin), num);
             res = Math.max(res, curMax);
         }
         return res;
