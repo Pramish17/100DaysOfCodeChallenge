1bb19d82c4eeb83ba5bd6e7bd70e998a6ef300da
Pramish Thapa <pramish.thapa17@gmail.com>
2024-12-21 23:18:28 +0000

    Partition labels code added

---
+++ src/main/java/PartitionLabels.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PartitionLabels {
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> lastIndex = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex.put(s.charAt(i), i);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        int size = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            size++;
+            end = Math.max(end, lastIndex.get(s.charAt(i)));
+
+            if (i == end) {
+                res.add(size);
+                size = 0;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        PartitionLabels solution = new PartitionLabels();
+        String s = "ababcbacadefegdehijhklij";
+        List<Integer> result = solution.partitionLabels(s);
+
+        System.out.println("Partition sizes: " + result);
+    }
+}
