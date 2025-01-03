9e2c35ef0bc92a1d99cf87e00c3832470c5dc05a
Pramish Thapa <pramish.thapa17@gmail.com>
2025-01-03 23:26:21 +0000

    SearchInRotatedSortedArray code added

---
+++ src/main/java/SearchInRotatedSortedArray.java
@@ -0,0 +1,39 @@
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (nums[m] == target) {
+                return m;
+            }
+
+            if (nums[l] <= nums[m]) {
+                if (target > nums[m] || target < nums[l]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                if (target < nums[m] || target > nums[r]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SearchInRotatedSortedArray solution = new SearchInRotatedSortedArray();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        int result = solution.search(nums, target);
+        System.out.println("Index of target " + target + ": " + result);
+    }
+}
