dba45175d991ac959a686e28eebda0abae019e3c
Pramish Thapa <pramish.thapa17@gmail.com>
2025-01-22 22:19:12 +0000

    Construct Binary Tree From Preorder and Inorder Traversal code added

---
+++ src/main/java/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,61 @@
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+
+    static int preIdx = 0;
+    static int inIdx = 0;
+
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+
+        ConstructBinaryTreeFromPreorderAndInorderTraversal solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal();
+        TreeNode root = solution.buildTree(preorder, inorder);
+
+        // Print the tree structure for verification (Inorder Traversal)
+        inorderTraversal(root);
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return dfs(preorder, inorder, Integer.MAX_VALUE);
+    }
+
+    private TreeNode dfs(int[] preorder, int[] inorder, int limit) {
+        if (preIdx >= preorder.length) return null;
+        if (inorder[inIdx] == limit) {
+            inIdx++;
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[preIdx++]);
+        root.left = dfs(preorder, inorder, root.val);
+        root.right = dfs(preorder, inorder, limit);
+        return root;
+    }
+
+    // Helper method to print Inorder traversal of the tree
+    public static void inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            System.out.print(root.val + " ");
+            inorderTraversal(root.right);
+        }
+    }
+
+    // Definition for a binary tree node.
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
